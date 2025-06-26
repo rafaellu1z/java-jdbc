85050f760a9d7b9efbdd2032476f524ffd7c2e47
Rafael Santana Luiz <rafael.s.luiz@gmail.com>
2025-06-26 20:25:26 -0300

    insert implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -27,5 +28,11 @@ public class Program {
         System.out.println("=== TEST 3: seller findAll ===");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
