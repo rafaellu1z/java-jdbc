2c72970b77e1c85a446faa97c00c1c9b52b79fe1
Rafael Santana Luiz <rafael.s.luiz@gmail.com>
2025-06-26 21:32:40 -0300

    DepartmentDaoJDBC and findbyId department

---
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,64 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.sql.*;
+import java.util.List;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement("SELECT Id, Name FROM department WHERE Id = ?");
+
+            st.setInt(1, id);
+
+            rs = st.executeQuery();
+
+            if(rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("Id"));
+                dep.setName(rs.getString("Name"));
+                return dep;
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
